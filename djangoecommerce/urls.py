cc9e04bdd0710e1f95acaf72788887e6f74163e1
vanros <vanessarodrigues.vj@gmail.com>
2017-03-11 00:21:27 -0300

    adding template index.html

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from core.views import index
 urlpatterns = [
+    url(r'^$',index),
     url(r'^admin/', admin.site.urls),
 ]
